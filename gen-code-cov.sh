c19fc10f62eea24038423f39e854b903c21bfc20
Vincent Gauthier <vgauthier@luxbulb.org>
2018-04-16 11:16:36 +0200

    update travis

---
+++ gen-code-cov.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd cmake-build-debug
+cmake .. -DENABLE_CODECOVERAGE=ON
+make 
+make test
+lcov --directory . --capture --output-file coverage.info
+lcov --remove coverage.info '/usr/*' '*/tests/*'  '/Library/Developer/*' --output-file coverage.info
+lcov --list coverage.info
+export CODECOV_TOKEN="5dd73d4a-b623-40c8-8926-766ee0d69711"
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
