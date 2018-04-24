50f76854672697d98ffd9e1a327a357a052a8a8a
Vincent Gauthier <vgauthier@luxbulb.org>
2018-04-24 19:27:50 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,105 @@
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+import sys, os
+import setuptools
+
+os.environ["CC"] = "clang"
+os.environ["CXX"] = "clang++"
+
+__version__ = '0.0.1'
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+ext_modules = [
+    Extension(
+        'pybstream',
+        ['src/interface/interface.cpp'],
+        include_dirs=[
+            "src/matrix",
+            "src/graph",
+            "src/interface",
+            "src/tinterval",
+            "src/utils",
+            "src/algorithms",
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True)
+        ],
+        language='c++'
+    ),
+]
+
+# As of Python 3.6, CCompiler has a `has_flag` method.
+# cf http://bugs.python.org/issue26689
+def has_flag(compiler, flagname):
+    """Return a boolean indicating whether a flag name is supported on
+    the specified compiler.
+    """
+    import tempfile
+    with tempfile.NamedTemporaryFile('w', suffix='.cpp') as f:
+        f.write('int main (int argc, char **argv) { return 0; }')
+        try:
+            compiler.compile([f.name], extra_postargs=[flagname])
+        except setuptools.distutils.errors.CompileError:
+            return False
+    return True
+
+def cpp_flag(compiler):
+    """Return the -std=c++[17] compiler flag.
+    """
+    if has_flag(compiler, '-std=c++1z'):
+        return '-std=c++1z'
+    else:
+        raise RuntimeError('Unsupported compiler -- at least C++17 support '
+                           'is needed!')
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append(cpp_flag(self.compiler))
+            if has_flag(self.compiler, '-fvisibility=hidden'):
+                opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+
+setup(
+    name='pybstream',
+    version=__version__,
+    author='Vincent Gauthier',
+    author_email='vgauthier@luxbulb.org',
+    packages=find_packages(),
+    url='https://github.com/pybind/python_example',
+    description='A test project using pybind11',
+    long_description='',
+    ext_modules=ext_modules,
+    install_requires=['pybind11>=2.2'],
+    cmdclass={'build_ext': BuildExt},
+    test_suite="tests.python_graph_tests.graph_tests",
+    zip_safe=False,
+)
