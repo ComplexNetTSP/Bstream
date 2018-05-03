ca6f0d90b3d8b3bbb1a3d097120263c2246166e7
Vincent Gauthier <vgauthier@luxbulb.org>
2018-05-03 17:56:20 +0200

    fix test case

--- tests/python_timeintervalset_tests/tintervalset_tests.py
+++ tests/python_timeintervalset_tests/tintervalset_tests.py
@@ -7,18 +7,18 @@ class TestTIntervalSet(unittest.TestCase):
     def test_TIntervalSet_constructor(self):
         """Test Constructor of TimeIntervalSet"""
         ti = pybstream.TimeIntervalSet()
-        (l,h) = ti.interval_def()
+        (l,h) = ti.definition()
         self.assertEqual(l, 0)
         self.assertEqual(h, 9223372036854775807)
 
-        ti.set_interval_def(0, 10)
-        (l,h) = ti.interval_def()
+        ti.set_definition(0, 10)
+        (l,h) = ti.definition()
         self.assertEqual(ti.definition_length(), 10)
         self.assertEqual(l, 0)
         self.assertEqual(h, 10)
 
         ti = pybstream.TimeIntervalSet(0, 10)
-        (l,h) = ti.interval_def()
+        (l,h) = ti.definition()
         self.assertEqual(ti.definition_length(), 10)
         self.assertEqual(l, 0)
         self.assertEqual(h, 10)
